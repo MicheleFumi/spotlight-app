cb3fed63ae460a0ab9d9fbf6107892388e71af2e
MicheleFumi <michele.fumi96@gmail.com>
2025-04-09 22:26:32 +0200

    add post creation

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,5 @@
 import { Image, Text, View, TouchableOpacity } from "react-native";
-import { styles } from "../../styles/auth.styles.ts";
+import { styles } from "../../styles/auth.styles";
 import { useAuth } from "@clerk/clerk-expo";
 
 export default function Index() {
@@ -8,7 +8,7 @@ export default function Index() {
     <View style={styles.container}>
       <TouchableOpacity onPress={() => signOut()}>
         <Text style={{ color: "white", flex: 1, justifyContent: "center" }}>
-          signout
+          signout!!!!
         </Text>
       </TouchableOpacity>
     </View>
