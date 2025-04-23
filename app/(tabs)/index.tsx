502a03a1e42b61954efc2c3a35970fab6a61b92f
MicheleFumi <michele.fumi96@gmail.com>
2025-04-23 12:15:46 +0200

    add posts layout

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,22 @@
-import { Image, Text, View, TouchableOpacity } from "react-native";
+import { Image, Text, View, TouchableOpacity, ScrollView } from "react-native";
 import { styles } from "../../styles/feed.styles";
 import { useAuth } from "@clerk/clerk-expo";
 import { Ionicons } from "@expo/vector-icons";
 import { COLORS } from "@/constants/theme";
+import { STORIES } from "@/constants/mock-data";
+import Story from "../components/Story";
+import { useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import Loader from "./Loader";
+import Post from "../components/Post";
 
 export default function Index() {
   const { signOut } = useAuth();
+
+  const posts = useQuery(api.posts.getFeedPost);
+  if (posts === undefined) return <Loader />;
+
+  if (posts.length === undefined) return <NoPostFound />;
   return (
     <View style={styles.container}>
       {/* HEADER SECTION */}
@@ -16,6 +27,37 @@ export default function Index() {
         </TouchableOpacity>
       </View>
       {/* STORIES SECTION (NOT-WORKING-REALLY) */}
+      <ScrollView showsVerticalScrollIndicator={false}>
+        {/* STORIES */}
+        <ScrollView
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          style={styles.storiesContainer}
+        >
+          {STORIES.map((story) => (
+            <Story key={story.id} story={story}></Story>
+          ))}
+        </ScrollView>
+
+        {posts.map((post) => (
+          <Post key={post._id} post={post} />
+        ))}
+      </ScrollView>
     </View>
   );
 }
+
+const NoPostFound = () => (
+  <View
+    style={{
+      flex: 1,
+      backgroundColor: COLORS.background,
+      justifyContent: "center",
+      alignItems: "center",
+    }}
+  >
+    <Text style={{ fontSize: 20, color: COLORS.primary }}>
+      Nothing To See Here!
+    </Text>
+  </View>
+);
--- app/components/Story.tsx
+++ app/components/Story.tsx
@@ -1,8 +1,23 @@
+import { styles } from "@/styles/feed.styles";
+import { TouchableOpacity, View, Image, Text } from "react-native";
+
 type Story = {
-  id: string;
+  id: number;
   username: string;
   avatar: string;
   hasStory: boolean;
 };
 
-export default function Story({ story });
+export default function Story({ story }: { story: Story }) {
+  return (
+    <TouchableOpacity style={styles.storyWrapper}>
+      <View style={[styles.storyRing, !story.hasStory && styles.noStory]}>
+        <Image
+          source={{ uri: story.avatar }}
+          style={styles.storyAvatar}
+        ></Image>
+      </View>
+      <Text style={styles.storyUsername}>{story.username}</Text>
+    </TouchableOpacity>
+  );
+}
